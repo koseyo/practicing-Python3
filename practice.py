e32ba8995c804c9813e518062b51edcdd970ee02
koseyo <koseyo08@yahoo.co.jp>
2021-10-31 15:28:10 +0900

    pushing practice file of Python3

---
+++ practice.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+word = "python"
+print(word[0])
+print(word[1:5])
+print(word[-1])
+print("**************")
+print(word[0:])
+print(word[0:1])
+print(word[1:])
+
+word = "j" + word[1:]
+print(word)
