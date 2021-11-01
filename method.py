28f9120ddcbc0b54eadb0958e4afb6e7038dee62
koseyo <koseyo08@yahoo.co.jp>
2021-11-01 16:57:20 +0900

    adding how to use dictionary's method

---
+++ method.py
@@ -0,0 +1,10 @@
+x = {"a": 1}
+y = x
+y["a"] = 1000
+print(x)
+print(y)
+
+x = {"a": 1}
+y = x.copy()
+print(x)
+print(y)
--- practice.py
+++ practice.py
@@ -1,10 +1,20 @@
 # coding: utf-8
 
-choose_from_two = ("A", "B", "C")
+d = {"x": 10, "y": 20}
+print(d)
+print(type(d))
 
-answer = []
-answer.append("A") #tuppleにappendはできない
-answer.append("C")
+print(d.keys()) #key's'
 
-print(choose_from_two)
-print(answer)
+print(d.values()) #value's'
+
+d2 = {"x": 1000, "j": 500}
+print(d2)
+
+d.update(d2)
+print(d)
+
+print(d.get("x"))
+print(d.get("z"))
+d.pop("x")
+print(d)
