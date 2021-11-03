65ebd8b127212851dc9cab768f8ebdad0b449610
koseyo <koseyo08@yahoo.co.jp>
2021-11-03 21:49:21 +0900

    add dictionary's method

--- method.py
+++ method.py
@@ -8,3 +8,19 @@ x = {"a": 1}
 y = x.copy()
 print(x)
 print(y)
+
+fruits = {
+    "apple": 100,
+    "banana": 200,
+    "orange": 300,
+}
+
+print(fruits["apple"])#リストは値を探すのに時間がかかる
+
+'''
+l = [
+    ["apple", 100],
+    ["banana", 200],
+    ["orange", 300],
+]
+'''
